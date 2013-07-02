2833e490cfb5d28f00751f8acc45e9b1fe1c8345
Seivan Heidari <seivan@kth.se>
2013-07-02 10:20:45 +0200

    Thank god for asserts.

--- Example/Example/SHSecondViewController.m
+++ Example/Example/SHSecondViewController.m
@@ -21,9 +21,10 @@
   __weak typeof(self) weakSelf = self;
 
   __block NSUInteger counter = 0;
-  SHControlEventBlock block = ^(UIControl * sender){
+  SHControlEventBlock counterBlock = ^(UIControl * sender){
     NSLog(@"SENDER : %@", sender);
     counter += 1;
+    //This block should only be called two times, even though it's added three times. 
     NSAssert(counter == 1, @"Counter should be one");
     if(counter > 1){
       NSAssert(counter == 2, @"Counter should be two");
@@ -32,26 +33,44 @@
 
   [self.btnFirst SH_addControlEvents:UIControlEventTouchDown withBlock:^(UIControl *sender) {
     [weakSelf.btnFirst removeFromSuperview];
-    NSSet * controlBlocks = self.btnFirst.SH_controlBlocks[@(UIControlEventTouchDown)];
-    NSAssert(weakSelf.btnFirst.SH_isTouchUpInsideEnabled == NO, @"Touch up inside should be enabled");
-    NSAssert(weakSelf.btnFirst.SH_controlBlocks.count == 1, @"There should be no events");
-    NSAssert(controlBlocks.count == 0, @"There should be no blocks");
-    [weakSelf.btnSecond SH_addControlEvents:UIControlEventTouchUpInside withBlock:block];
-    [weakSelf.btnSecond SH_addControlEvents:UIControlEventTouchDown withBlock:block];
-    [weakSelf.btnSecond SH_addControlEvents:UIControlEventTouchDown withBlock:^(UIControl *sender) {
+
+    //When first button is removed - all the controls should be gone.
+    double delayInSeconds = 2.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+      NSSet * controlBlocks = weakSelf.btnFirst.SH_controlBlocks[@(UIControlEventTouchDown)];
+      NSAssert(weakSelf.btnFirst.SH_isTouchUpInsideEnabled == NO, @"Touch up inside should not be enabled");
+      NSAssert(weakSelf.btnFirst.SH_controlBlocks.count == 0, @"There should be no events");
+      NSAssert(controlBlocks.count == 0, @"There should be no blocks");
       
-    }];
-    NSSet * controlTouchDownBlocks = self.btnFirst.SH_controlBlocks[@(UIControlEventTouchDown)];
-    NSSet * controlTouchUpInsideBlocks = self.btnFirst.SH_controlBlocks[@(UIControlEventTouchUpInside)];
-    NSAssert(weakSelf.btnFirst.SH_isTouchUpInsideEnabled == YES, @"Touch up inside should be enabled");
-    NSAssert(weakSelf.btnFirst.SH_controlBlocks.count == 2, @"There should be two events");
-    NSAssert(controlTouchDownBlocks.count == 0, @"There should be no blocks");
-    NSAssert(controlTouchUpInsideBlocks.count == 0, @"There should be no blocks");
+      //Creating new controls for the second button, since the block would be a duplicate, it will only add it once.
+      [weakSelf.btnSecond SH_addControlEvents:UIControlEventTouchUpInside withBlock:counterBlock];
+      [weakSelf.btnSecond SH_addControlEvents:UIControlEventTouchUpInside withBlock:counterBlock];
+      
+      //Add the same block here to touchDown
+      [weakSelf.btnSecond SH_addControlEvents:UIControlEventTouchDown withBlock:counterBlock];
+      
+      //Second block
+      [weakSelf.btnSecond SH_addControlEvents:UIControlEventTouchDown withBlock:^(UIControl *sender) {
+        
+      }];
+      
+      //Ensure second button has the proper controls, two events, one block per event
+      NSSet * controlTouchDownBlocks = self.btnFirst.SH_controlBlocks[@(UIControlEventTouchDown)];
+      NSSet * controlTouchUpInsideBlocks = self.btnFirst.SH_controlBlocks[@(UIControlEventTouchUpInside)];
+      
+      NSAssert(weakSelf.btnSecond.SH_isTouchUpInsideEnabled == YES, @"Touch up inside should be enabled");
+      NSAssert(weakSelf.btnSecond.SH_controlBlocks.count == 2, @"There should be two events");
+      NSAssert(controlTouchDownBlocks.count == 2, @"There should be one block");
+      NSAssert(controlTouchUpInsideBlocks.count == 1, @"There should be one block");
+
+    });
 
   }];
-
+  
+  //Ensure first button has the proper controls
   NSSet * controlBlocks = self.btnFirst.SH_controlBlocks[@(UIControlEventTouchDown)];
-  NSAssert(self.btnFirst.SH_isTouchUpInsideEnabled == NO, @"Touch up inside should be enabled");
+  NSAssert(self.btnFirst.SH_isTouchUpInsideEnabled == NO, @"Touch up inside should not be enabled");
   NSAssert(self.btnFirst.SH_controlBlocks.count == 1, @"There should be one event");
   NSAssert(controlBlocks.count == 1, @"There should be one block");
 
