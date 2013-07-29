dbda1fb357f486c0939fe628cfc05c7133e8ceb3
Seivan Heidari <seivan@kth.se>
2013-07-29 03:21:00 +0200

    Push test build

--- Example/ExampleTests/SHActionSheetBlocksCallbacks.m
+++ Example/ExampleTests/SHActionSheetBlocksCallbacks.m
@@ -1,49 +1,44 @@
-////
-////  ExampleTests.m
-////  ExampleTests
-////
-////  Created by Seivan Heidari on 7/27/13.
-////  Copyright (c) 2013 Seivan Heidari. All rights reserved.
-////
 //
+//  ExampleTests.m
+//  ExampleTests
 //
-//#import "SHActionSheetBlocksSuper.h"
-//@interface SHActionSheetBlocksFullTests : SHActionSheetBlocksSuper
-//@end
+//  Created by Seivan Heidari on 7/27/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
 //
-//
-//@interface SHActionSheetBlocksFullTests ()
-//@end
-//
-//
-//@implementation SHActionSheetBlocksFullTests
-//
-//-(void)setUp; {
-//  [super setUp];
-//  
-//  self.block = ^(NSUInteger theButtonIndex) {
-//    
-//  };
-//  
-//  self.vc        = UIViewController.new;
-//
-//  
-//
-//}
-//
-//-(void)tearDown; {
-//  [super tearDown];
-//}
-//
-//-(void)testTitle; {
-//  STAssertNotNil(self.sheet.title, nil);
-//  STAssertNotNil(self.sheetFull.title, nil);
-//}
-//
-//-(void)testButton; {
-//  
-//  STAssertEquals(self.sheet.numberOfButtons, 4U, nil);
-//}
-//
-//
-//@end
+
+
+#import "SHActionSheetBlocksSuper.h"
+@interface SHActionSheetBlocksCallbacks : SHActionSheetBlocksSuper
+@end
+
+
+
+
+@implementation SHActionSheetBlocksCallbacks
+
+-(void)setUp; {
+  [super setUp];
+  
+  
+  self.block = ^(NSUInteger theButtonIndex) {
+    
+  };
+  
+  self.vc        = UIViewController.new;
+  self.sheet     = [UIActionSheet SH_actionSheetWithTitle:@"Title"];
+  
+  
+}
+
+-(void)tearDown; {
+  [super tearDown];
+}
+
+-(void)testSeparateBlocksPerButton; {
+  [self.sheet SH_addButtonWithTitle:@"Button1" withBlock:^(NSUInteger theButtonIndex) {
+    
+  }];
+}
+
+
+@end
