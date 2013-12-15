c75a792a53c1d3df8500f97d9aa5d65c8972f9d3
Seivan Heidari <seivan@kth.se>
2013-12-15 16:34:58 +0100

    This is good.

--- Sample/Library/SHAppDelegate.m
+++ Sample/Library/SHAppDelegate.m
@@ -12,10 +12,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"WORKS?");
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    self.window.rootViewController = UIViewController.new;
     return YES;
 }
 
