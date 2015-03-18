28f154f6db0ba327df2d808b14f6d14407e7b1fb
Seivan Heidari <seivan.heidari@icloud.com>
2015-03-18 04:56:24 +0100

    Install with new Cocoapods.

---
+++ TestsAndSample/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// KIF
+#define COCOAPODS_POD_AVAILABLE_KIF
+#define COCOAPODS_VERSION_MAJOR_KIF 3
+#define COCOAPODS_VERSION_MINOR_KIF 2
+#define COCOAPODS_VERSION_PATCH_KIF 0
+
+// KIF/XCTest
+#define COCOAPODS_POD_AVAILABLE_KIF_XCTest
+#define COCOAPODS_VERSION_MAJOR_KIF_XCTest 3
+#define COCOAPODS_VERSION_MINOR_KIF_XCTest 2
+#define COCOAPODS_VERSION_PATCH_KIF_XCTest 0
+
+// SHActionSheetBlocks
+#define COCOAPODS_POD_AVAILABLE_SHActionSheetBlocks
+#define COCOAPODS_VERSION_MAJOR_SHActionSheetBlocks 2
+#define COCOAPODS_VERSION_MINOR_SHActionSheetBlocks 3
+#define COCOAPODS_VERSION_PATCH_SHActionSheetBlocks 1
+
+// SHTestCaseAdditions
+#define COCOAPODS_POD_AVAILABLE_SHTestCaseAdditions
+#define COCOAPODS_VERSION_MAJOR_SHTestCaseAdditions 1
+#define COCOAPODS_VERSION_MINOR_SHTestCaseAdditions 4
+#define COCOAPODS_VERSION_PATCH_SHTestCaseAdditions 1
+
+// SHTestCaseAdditions/XCTest
+#define COCOAPODS_POD_AVAILABLE_SHTestCaseAdditions_XCTest
+#define COCOAPODS_VERSION_MAJOR_SHTestCaseAdditions_XCTest 1
+#define COCOAPODS_VERSION_MINOR_SHTestCaseAdditions_XCTest 4
+#define COCOAPODS_VERSION_PATCH_SHTestCaseAdditions_XCTest 1
+
