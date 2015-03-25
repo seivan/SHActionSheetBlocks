9e712d36cde032a84ad1f456ecfa5a53c9ec022d
Seivan Heidari <seivan.heidari@icloud.com>
2015-03-25 21:20:29 +0100

    Update sample.

--- TestsAndSample/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ TestsAndSample/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -22,7 +22,7 @@
 #define COCOAPODS_POD_AVAILABLE_SHActionSheetBlocks
 #define COCOAPODS_VERSION_MAJOR_SHActionSheetBlocks 2
 #define COCOAPODS_VERSION_MINOR_SHActionSheetBlocks 3
-#define COCOAPODS_VERSION_PATCH_SHActionSheetBlocks 1
+#define COCOAPODS_VERSION_PATCH_SHActionSheetBlocks 3
 
 // SHTestCaseAdditions
 #define COCOAPODS_POD_AVAILABLE_SHTestCaseAdditions
